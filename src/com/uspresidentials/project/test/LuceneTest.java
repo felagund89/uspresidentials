493dbfe1df325f81fc2d5bda44dd6aceb893264b
Alessio Campanelli <alessio.campanelli@mobilesoft.it>
2016-06-11 11:52:33 +0200

    utilizzo di:     - jsoup per lo scraping     - twitter4J per la creazione e lettura dei tweet

--- src/com/uspresidentials/project/test/LuceneTest.java
+++ src/com/uspresidentials/project/test/LuceneTest.java
@@ -18,25 +18,30 @@ import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.LockObtainFailedException;
 import org.apache.lucene.util.Version;
+import org.jsoup.Jsoup;
+import org.jsoup.select.Elements;
+
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterObjectFactory;
 
 public class LuceneTest {
 	
 	final static String PATH_DEBATES = "/Users/alessiocampanelli/Desktop/debates";
+  //final static String PATH_DEBATES = "/home/felagund89/Scrivania/Progetto web and social/debates";
 	
-	public static void main(String []args) throws CorruptIndexException, LockObtainFailedException, IOException{
-		
-		//commit by Alessio
+	public static void main(String []args) throws CorruptIndexException, LockObtainFailedException, IOException, TwitterException{
 		createIndex();
 	}
 	
-	public static void createIndex() throws CorruptIndexException, LockObtainFailedException, IOException {
+	public static void createIndex() throws CorruptIndexException, LockObtainFailedException, IOException, TwitterException {
 		
 		Analyzer analyzer = new StandardAnalyzer();	
 		Directory indexDir = FSDirectory.open(new File("/Users/alessiocampanelli/Desktop/resultQuery"));
 		IndexWriterConfig config = new IndexWriterConfig(Version.LATEST, new StandardAnalyzer());
 		IndexWriter indexWriter = new IndexWriter(indexDir, config);
 
-		Document doc = new Document();
+		Document doc = new Document();     //Lucene document
 		
 		File dir = new File(PATH_DEBATES);
 		File[] files = dir.listFiles();
@@ -44,10 +49,18 @@ public class LuceneTest {
 		File currentFile;
 		String currentContent;
 		
-		//for(int i=0;i<files.length;i++){
-			currentFile = files[0];
+		//for(int i=0;i<files.length;i++){    //ciclo su ogni File del dataset
+		
+			currentFile = files[1];
 			currentContent = readContentFile(currentFile);
-			System.out.println("currentContent: " + currentContent);
+	
+			//scraping di un singolo oggetto in un file del dataset
+			org.jsoup.nodes.Document docJsoup = Jsoup.parse(currentContent);
+			Elements newsHeadlines = docJsoup.select("p");					   
+			
+			//creazione dell'oggetto Tweet4J passando in input il contenuto json del tag 'p'  - ora prendo il primo tweet
+			String jsonContent = newsHeadlines.get(0).text();                 
+			Status status = TwitterObjectFactory.createStatus(jsonContent);
 		//}
 	}
 	
