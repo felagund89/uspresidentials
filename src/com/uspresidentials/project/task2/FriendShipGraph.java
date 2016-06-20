593e4268227f532c8357c86922332f47b8ea6f7a
Alessio Campanelli <alessio.campanelli@mobilesoft.it>
2016-06-21 00:01:26 +0200

    friendship modificato

--- src/com/uspresidentials/project/task2/FriendShipGraph.java
+++ src/com/uspresidentials/project/task2/FriendShipGraph.java
@@ -9,6 +9,7 @@ import twitter4j.Twitter;
 import twitter4j.TwitterException;
 import twitter4j.TwitterFactory;
 import twitter4j.User;
+import twitter4j.auth.AccessToken;
 
 public class FriendShipGraph {
 
@@ -26,10 +27,12 @@ public class FriendShipGraph {
 	      IDs ids;
 	      Twitter twitter = TwitterFactory.getSingleton();
 	      twitter.setOAuthConsumer("1HERcFVCy5SkpI23hl3FRpJy3", "5PFPlMp3NAsAT1Qbrk1RStXWLMX795ghSUfubtwILl5vR2keyW");
+	      AccessToken accessToken = new AccessToken("2977694199-o286ySyyQbCTJsMXcxSfoeSwQ6CkVGQSNl8ILMO", "SKo5MvolhkJxmoG3ADgb2tzW5oOFV7p6A44hmcHY1Pzz1");
+	      twitter.setOAuthAccessToken(accessToken);
 	      
 	      System.out.println("Listing followers's ids.");
 	      do {
-	              ids = twitter.getFollowersIDs("username", cursor);
+	              ids = twitter.getFollowersIDs("felagund89", cursor);
 	          for (long id : ids.getIDs()) {
 	              System.out.println(id);
 	              User user = twitter.showUser(id);
