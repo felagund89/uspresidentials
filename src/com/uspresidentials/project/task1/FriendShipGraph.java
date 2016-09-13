2ea735466b24ab4b62821e633ceea5bc94e8abbb
antonioa89 <antonioa89@hotmail.it>
2016-09-13 10:07:35 +0200

    modifica algoritmo friendship graph v6
    aggiunta credenziali di un amico, va implementato un modo per scorrere tra vari account per ottimizzare i tempi

--- src/com/uspresidentials/project/task1/FriendShipGraph.java
+++ src/com/uspresidentials/project/task1/FriendShipGraph.java
@@ -38,26 +38,42 @@ import twitter4j.auth.AccessToken;
 public class FriendShipGraph {
 
 	final long MAX_USERS = 500;
-	final static String PATH_FILE_UTENTI_ID = "/home/felagund89/Scrivania/utentiTwitter.txt";
+	//final static String PATH_FILE_UTENTI_ID = "/home/felagund89/Scrivania/utentiTwitter.txt";
+	final static String PATH_FILE_UTENTI_ID = "D:/Users/aacciard/Desktop/utentiTwitter.txt";
 	static int NUMERO_UTENTI;
 	static Boolean isPrivateFriends=false;
+	static String outhConsumerKeyClaudio="KqYc0PQ1seDR36glxMwxgSq5O";
+	static String outhConsumerSecretClaudio ="v05RdurBZRCVmeKc6Tig4eeWdghQXrvubtb1GR7PTOFSKfjWnK";
+	static String accesTOkenClaudioString="access token: 469453578-MeraM7YtOFHKYAiAVBNRG676ZHEgnHAUqaYVGx35";
+	static String accestokenSecretCLaudioString="OFx87bY2GSxaTkIHKNR9ddST8XChoTg4M4aHQw6PZRA91";
 	
+
+	static String outhConsumerKeyAntonio="1HERcFVCy5SkpI23hl3FRpJy3";
+	static String outhConsumerSecretAntonio ="5PFPlMp3NAsAT1Qbrk1RStXWLMX795ghSUfubtwILl5vR2keyW";
+	static String accesTOkenAntonioString="2977694199-o286ySyyQbCTJsMXcxSfoeSwQ6CkVGQSNl8ILMO";
+	static String accestokenSecretAntonioString="SKo5MvolhkJxmoG3ADgb2tzW5oOFV7p6A44hmcHY1Pzz1";
+	
+	static String outhConsumerKeyAlessia="FzkBBBPes89nojrjfiVgzyrmy";
+	static String outhConsumerSecretAlessia ="MffW9BNFSSo9eKPHGddokTREiUoJXAu93hpOVHcqGMXq8E6Dor";
+	static String accesTOkenAlessiaString="872321179-FoPaVqfdIf7JP4GwrXRP2TCJSncItXQK6oTBYioW";
+	static String accestokenSecretAlessiaString="L1gusbWbNvl52CQ965JZpvhCnsHli8royqgVnQllwhLYb";
 	public static void main(String[] args) throws IOException, TwitterException {
 		// TODO Auto-generated method stub
 
 		//Authentication.InitializeTwitterObj("");
 		  Twitter twitter = TwitterFactory.getSingleton();
-	      twitter.setOAuthConsumer("1HERcFVCy5SkpI23hl3FRpJy3", "5PFPlMp3NAsAT1Qbrk1RStXWLMX795ghSUfubtwILl5vR2keyW");
-	      AccessToken accessToken = new AccessToken("2977694199-o286ySyyQbCTJsMXcxSfoeSwQ6CkVGQSNl8ILMO", "SKo5MvolhkJxmoG3ADgb2tzW5oOFV7p6A44hmcHY1Pzz1");
+//	      twitter.setOAuthConsumer("1HERcFVCy5SkpI23hl3FRpJy3", "5PFPlMp3NAsAT1Qbrk1RStXWLMX795ghSUfubtwILl5vR2keyW");
+//	      AccessToken accessToken = new AccessToken("2977694199-o286ySyyQbCTJsMXcxSfoeSwQ6CkVGQSNl8ILMO", "SKo5MvolhkJxmoG3ADgb2tzW5oOFV7p6A44hmcHY1Pzz1");
+		  twitter.setOAuthConsumer(outhConsumerKeyAntonio, outhConsumerSecretAntonio);
+	      AccessToken accessToken = new AccessToken(accesTOkenAntonioString, accestokenSecretAntonioString);
+
 	      twitter.setOAuthAccessToken(accessToken);
-	      long idUser = twitter.getId() ;
+	      //long idUser = twitter.getId() ;
 	      //recupero gli amici a partire da un account specifico 180 amici alla volta, trovare modo per automatizzare il crawling 
 	      //per trovare e salvare tutti gli amici su file
-		  getGlobalFriendship(twitter,"Alex_Campanels",1676105413L, -1,-1);
+		  getGlobalFriendship(twitter);
 
-	      
-	     
-			
+	
 		//Creo grafo e cerco la componente connessa piu grande
 	    //ListenableDirectedGraph<String, DefaultEdge> myGraph = (ListenableDirectedGraph<String, DefaultEdge>) FriendShipGraph.createGraph();
 	    //dato un grafo cerco la componente connessa piu grande
@@ -69,7 +85,7 @@ public class FriendShipGraph {
 	}
 	
 	
-	public static void getGlobalFriendship(Twitter twitter, String userName, long idUser, long cursor, long currentCursor) throws TwitterException, FileNotFoundException, IOException{
+	public static void getGlobalFriendship(Twitter twitter) throws TwitterException, FileNotFoundException, IOException{
 		
 		
 	     try (BufferedReader br = new BufferedReader(new FileReader(PATH_FILE_UTENTI_ID))) {
@@ -77,14 +93,15 @@ public class FriendShipGraph {
 			    
 			    while ((line = br.readLine()) != null  ) {
 			    	//definiamo l'id utente, split[0] è il nome utente
-
+			    	
+			    	 String userName;
+			    	 long idUser; 
 			    	 idUser= Long.parseLong((line.split(";")[1]));
 					 IDs ids = twitter.getFriendsIDs(idUser, -1);  //calcola il numero totale degli amici relativi a idUser 
-
 			    	 userName=line.split(";")[0];
 			    	 isPrivateFriends=false;
 			    	 int numberOfFriends= ids.getIDs().length;
-			         System.out.println("Utente " +userName+"ha "+numberOfFriends+ " amici.");
+			         System.out.println("Utente " +userName+" ha "+numberOfFriends+ " amici.");
 		             //scrivo su file il nome dell'utente che stiamo analizzando
 			         writeUsersOnFile(userName+":");
 				     getFriendShipRecursive(twitter,userName,idUser, -1,numberOfFriends);
@@ -145,7 +162,7 @@ public class FriendShipGraph {
 			            }
 							    
 					    try {
-					    	
+					    	twitter = getFastCredentialsForQuery(twitter);
 							int toSleep = twitter.getRateLimitStatus().get("/friends/list").getSecondsUntilReset() + 1;
 							System.out.println("Sleeping for " + toSleep + " seconds.");
 							Thread.sleep(toSleep * 1000);
@@ -165,6 +182,54 @@ public class FriendShipGraph {
 	    
 	      
 	    //crea il grafo leggendo tale file
+	}
+	
+	
+	private static Twitter getFastCredentialsForQuery(Twitter twitter) throws TwitterException{
+		//cambia le credenziali di attesa e setta le credenziali col tempo di attesa minore
+		
+		//verifico tra i vari account il tempo minore e ritorno l'oggetto twitter settato correttamente
+		
+		//account antonio
+		twitter.setOAuthConsumer(outhConsumerKeyAntonio, outhConsumerSecretAntonio);
+	    AccessToken accessToken = new AccessToken(accesTOkenAntonioString, accestokenSecretAntonioString);
+	    twitter.setOAuthAccessToken(accessToken);
+		int toSleep = twitter.getRateLimitStatus().get("/friends/list").getSecondsUntilReset() + 1;
+
+		//account claudio
+		twitter.setOAuthConsumer(outhConsumerKeyClaudio, outhConsumerSecretClaudio);
+	    accessToken = new AccessToken(accesTOkenClaudioString, accestokenSecretCLaudioString);
+	    twitter.setOAuthAccessToken(accessToken);
+		int toSleep2 = twitter.getRateLimitStatus().get("/friends/list").getSecondsUntilReset() + 1;
+		
+		//account alessia
+		twitter.setOAuthConsumer(outhConsumerKeyAlessia, outhConsumerSecretAlessia);
+	    accessToken = new AccessToken(accesTOkenAlessiaString, accestokenSecretAlessiaString);
+	    twitter.setOAuthAccessToken(accessToken);
+		int toSleep3 = twitter.getRateLimitStatus().get("/friends/list").getSecondsUntilReset() + 1;
+		
+		if(toSleep2 < toSleep){
+			
+			if(toSleep2 > toSleep3){
+				return twitter;
+			}else {
+				twitter.setOAuthConsumer(outhConsumerKeyClaudio, outhConsumerSecretClaudio);
+			    accessToken = new AccessToken(accesTOkenClaudioString, accestokenSecretCLaudioString);
+			    twitter.setOAuthAccessToken(accessToken);
+			    return twitter;
+			}
+		}else if(toSleep < toSleep3) {
+			twitter.setOAuthConsumer(outhConsumerKeyAntonio, outhConsumerSecretAntonio);
+		    accessToken = new AccessToken(accesTOkenAntonioString, accestokenSecretAntonioString);
+		    twitter.setOAuthAccessToken(accessToken);
+			return twitter;
+		}else{	
+			return twitter;
+		}
+		
+		
+		
+
 	}
 	
 	
@@ -243,7 +308,7 @@ public class FriendShipGraph {
 //		PrintWriter writer = new PrintWriter("/Users/alessiocampanelli/Desktop/friendshipTwitter.txt", "UTF-8");
 //		PrintWriter writer = new PrintWriter("/home/felagund89/Scrivania/friendshipTwitter.txt", "UTF-8");
 
-		PrintWriter writer = new PrintWriter(new FileOutputStream(new File("/home/felagund89/Scrivania/friendshipTwitterAll.txt"),true));
+		PrintWriter writer = new PrintWriter(new FileOutputStream(new File("D:/Users/aacciard/Desktop/friendshipTwitterAll.txt"),true));
 		
 		writer.println(content);
 		writer.close();
