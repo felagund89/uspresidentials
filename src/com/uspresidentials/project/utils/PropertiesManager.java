f8076005b4d210f336223292b5ecfacd2a594556
antonioa89 <antonioa89@hotmail.it>
2016-09-18 15:33:52 +0200

    manager per il file di properties

---
+++ src/com/uspresidentials/project/utils/PropertiesManager.java
@@ -0,0 +1,45 @@
+package com.uspresidentials.project.utils;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+public class PropertiesManager {
+
+	public static String getPropertiesFromFile(String propName) {
+
+		Properties propFile = new Properties();
+		InputStream input = null;
+		String propValue = null;
+
+		try {
+
+			String filename = "/utils/ConfigPropertiesTwitter.properties";
+			input = PropertiesManager.class.getClassLoader()
+					.getResourceAsStream(filename);
+			if (input == null) {
+				System.out.println("File non trovato " + filename);
+				return "0";
+			}
+
+			// carica il file di properties
+			propFile.load(input);
+
+			// prende la property ricercata
+			propValue = propFile.getProperty(propName);
+			System.out.println("Valore della properties: " + propValue);
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		} finally {
+			if (input != null) {
+				try {
+					input.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return propValue;
+	}
+}
