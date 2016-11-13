9d7fcc00a2bfa950db2c5043fb4c8b579585c870
antonioa89 <antonioa89@hotmail.it>
2016-11-13 12:49:44 +0100

    implementazione jaccard
    -effettuato il calcolo di jaccard sulle coppie di parole nell'intero dataset per ogni candidato
    -vanno fatte 4 chiamate per tutti i candidati, e vanno calcolate le coppie con l'indice più alto. Va gestito il caso in cui l'indice sia zero.
    -aspettiamo anche cosa dice il prof sulla questione jaccard.

--- src/com/uspresidentials/project/lucene/LuceneCore.java
+++ src/com/uspresidentials/project/lucene/LuceneCore.java
@@ -23,18 +23,22 @@ import org.apache.lucene.document.StringField;
 import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.CorruptIndexException;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.Fields;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
-import org.apache.lucene.index.IndexableField;
+import org.apache.lucene.index.MultiFields;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.index.Terms;
 import org.apache.lucene.index.TermsEnum;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.BooleanClause;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TermQuery;
 import org.apache.lucene.search.TopDocs;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
@@ -44,15 +48,14 @@ import org.apache.lucene.util.Version;
 import org.jsoup.Jsoup;
 import org.jsoup.select.Elements;
 
-import com.uspresidentials.project.entity.TweetInfoEntity;
-import com.uspresidentials.project.entity.TweetsEntity;
-import com.uspresidentials.project.entity.WordEntity;
-
 import twitter4j.Status;
 import twitter4j.TwitterException;
 import twitter4j.TwitterObjectFactory;
 import twitter4j.User;
-import twitter4j.UserList;
+
+import com.uspresidentials.project.entity.TweetInfoEntity;
+import com.uspresidentials.project.entity.TweetsEntity;
+import com.uspresidentials.project.entity.WordEntity;
 
 public class LuceneCore {
 
@@ -404,7 +407,7 @@ public class LuceneCore {
  	    QueryParser qp = new QueryParser(fieldForQuery, new StandardAnalyzer());
  	    
  	    Query q1 = qp.parse(queryLucene);
- 	    TopDocs hits = searcher.search(q1, 100000);
+ 	    TopDocs hits = searcher.search(q1, 1000);
  	    
 //		loggerUsersAndTweets.info("##### "+hits.totalHits + " Docs found for the query \"" + q1.toString() + "\"");
 
@@ -413,7 +416,7 @@ public class LuceneCore {
  	    
  	    //Nel primo elemento della mappa inserisco il numero dei documenti trovati per quel determinato candidato.
  	    //gli elementi successivi della mappa sono tutti i termini trovati e la loro frequenza in tutti i documenti
- 	    frequencies.put("NumberOfDocs", hits.totalHits);
+ 	    //frequencies.put("NumberOfDocs", hits.totalHits); non serve
  	    for (ScoreDoc sd : hits.scoreDocs) {
  	    	Document d = searcher.doc(sd.doc);
  	        boolean countDoc=false;
@@ -446,8 +449,8 @@ public class LuceneCore {
 		 		    WordEntity word = new WordEntity();
 
 		 		    word.setWord(term);
-		 		    word.setNumDocOcc(1);
-		 		    word.setTotalOcc(1);
+		 		    word.setNumDocOcc(1.0);
+		 		    word.setTotalOcc(1.0);
 		 		    words.add(word);
 	 		   }
 	 		   
@@ -459,7 +462,7 @@ public class LuceneCore {
  	      
  	      
  	    }
- 	      	    
+ 	    System.out.println("fine getTerms"); 	    
  	    return words;
 	 }
 	
@@ -536,5 +539,78 @@ public class LuceneCore {
 	        indexWriter.close();
 	    }
 	}
+	
+	
+
+	/** PROVA PROVA PROVA
+	*  Map term to a fix integer so that we can build document matrix later.
+	*  It's used to assign term to specific row in Term-Document matrix
+	*/
+	public static Map<String, Integer> computeTermIdMap(String pathIndexer) throws IOException {
+	   
+		IndexReader reader = DirectoryReader.open(FSDirectory.open(new File(pathIndexer))); 
+
+	    searcher = new IndexSearcher(reader);
+		Map<String,Integer> termIdMap = new HashMap<String,Integer>();
+	    int id = 0;
+	    Fields fields = MultiFields.getFields(reader);
+	    Terms terms = fields.terms("tweetTextIndexed");
+	    TermsEnum itr = terms.iterator(null);
+	    BytesRef term = null;
+	    while ((term = itr.next()) != null) {               
+	        String termText = term.utf8ToString();              
+	        if (termIdMap.containsKey(termText))
+	            continue;
+	        //System.out.println(termText); 
+	        termIdMap.put(termText, id++);
+	        System.out.println(termText +" "+ id); 
+
+	    }
+
+	    return termIdMap;
+	}
+
+	/**
+	*  build term-document matrix for the given directory
+	*/
+//	public RealMatrix buildTermDocumentMatrix () throws IOException {
+//	    //iterate through directory to work with each doc
+//	    int col = 0;
+//	    int numDocs = countDocs(corpus);            //get the number of documents here      
+//	    int numTerms = termIdMap.size();    //total number of terms     
+//	    RealMatrix tdMatrix = new Array2DRowRealMatrix(numTerms, numDocs);
+//
+//	    for (File f : corpus.listFiles()) {
+//	        if (!f.isHidden() && f.canRead()) {
+//	            //I build term document matrix for a subset of corpus so
+//	            //I need to lookup document by path name. 
+//	            //If you build for the whole corpus, just iterate through all documents
+//	            String path = f.getPath();
+//	            BooleanQuery pathQuery = new BooleanQuery();
+//	            pathQuery.add(new TermQuery(new Term("path", path)), BooleanClause.Occur.SHOULD);
+//	            TopDocs hits = searcher.search(pathQuery, 1);
+//
+//	            //get term vector
+//	            Terms termVector = reader.getTermVector(hits.scoreDocs[0].doc, "contents");
+//	            TermsEnum itr = termVector.iterator(null);
+//	            BytesRef term = null;
+//
+//	            //compute term weight
+//	            while ((term = itr.next()) != null) {               
+//	                String termText = term.utf8ToString();              
+//	                int row = termIdMap.get(termText);
+//	                long termFreq = itr.totalTermFreq();
+//	                long docCount = itr.docFreq();
+//	                double weight = computeTfIdfWeight(termFreq, docCount, numDocs);
+//	                tdMatrix.setEntry(row, col, weight);
+//	            }
+//	            col++;
+//	        }
+//	    }       
+//	    return tdMatrix;
+//	}
+	
+	
+	
 	
 }
